11fc9b7a50a98b45172f6d4f8bbc71f0657e0428
Jica Monsanto <jica.p.monsanto@gmail.com>
2016-01-30 17:27:25 +0800

    - added UI scene and scripts - added UI and Monsters scene to build settings

---
+++ GGJ2016/GGJ2016/Assets/Game/Scripts/Scenes/UIRoot.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Common;
+using Common.Signal;
+
+namespace Game {
+
+	public class UIRoot : GameScene {
+
+		[SerializeField]
+		private GameObject KillIconTemplate;
+		[SerializeField]
+		private Transform KillIconsParent;
+
+		private List<GameObject> KillIcons = new List<GameObject> ();
+		private List<GameObject> UnusedKillIcons = new List<GameObject> ();
+
+		protected override void Awake() {
+			base.Awake();
+
+			// temp for test
+			LoadSceneAdditive (EScene.Monsters);
+
+			GameSignals.ON_MONSTER_DEAD.AddListener (OnMonsterDead);
+		}
+
+		void OnDestroy()
+		{
+			GameSignals.ON_MONSTER_DEAD.RemoveListener (OnMonsterDead);
+		}
+
+		public void RestartGame()
+		{
+			ClearKillIcons ();
+			GameSignals.START_GAME.Dispatch ();
+		}
+
+		private void ClearKillIcons()
+		{
+			foreach (GameObject killIcon in KillIcons) {
+				killIcon.SetActive (false);
+				UnusedKillIcons.Add(killIcon);
+			}
+			KillIcons.Clear ();
+		}
+
+		private GameObject GetKillIcon()
+		{
+			if (UnusedKillIcons.Count == 0) {
+				GameObject killIcon = Instantiate (KillIconTemplate) as GameObject;
+				killIcon.transform.SetParent (KillIconsParent);
+				return killIcon;
+			}
+			return UnusedKillIcons [0];
+		}
+
+		private void OnMonsterDead(ISignalParameters @params)
+		{
+			GameObject killIcon = GetKillIcon ();
+			killIcon.SetActive (true);
+		}
+	}
+
+}
