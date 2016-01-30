28501cd51390ce8bcf8807e763d860e1f5a63be2
Carlos Mapúa <carlos.mapua@gmail.com>
2016-01-30 16:31:54 +0800

    + added gesture manager and test scene + changed TouchKit execution order

---
+++ GGJ2016/GGJ2016/Assets/Game/Scripts/GestureManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Game
+{
+    public class GestureManager : MonoBehaviour
+    {
+        TKSwipeRecognizer swipeRecognizer;
+        TKTapRecognizer tapRecognizer;
+        TKPinchRecognizer pinchRecognizer;
+        TKLongPressRecognizer longPressRecognizer;
+
+        private void Start()
+        {
+            OnGameStarted();
+        }
+
+        void InitializeRecognizers()
+        {
+            swipeRecognizer = new TKSwipeRecognizer();
+            swipeRecognizer.gestureRecognizedEvent += OnSwipeRecognized;
+            TouchKit.addGestureRecognizer(swipeRecognizer);
+
+            tapRecognizer = new TKTapRecognizer();
+            tapRecognizer.gestureRecognizedEvent += OnTapRecognized;
+            TouchKit.addGestureRecognizer(tapRecognizer);
+            
+            pinchRecognizer = new TKPinchRecognizer();
+            pinchRecognizer.gestureRecognizedEvent += OnPinchRecognized;
+            TouchKit.addGestureRecognizer(pinchRecognizer);
+
+            longPressRecognizer = new TKLongPressRecognizer();
+            longPressRecognizer.gestureRecognizedEvent += OnLongTapRecognized;
+            longPressRecognizer.gestureCompleteEvent += OnLongTapFinished;
+            TouchKit.addGestureRecognizer(longPressRecognizer);
+
+            Debug.Log("recognizers successfully initialized");
+        }
+
+        void DisableRecognizers()
+        {
+            TouchKit.removeAllGestureRecognizers();
+        }
+
+        void OnGameStarted()
+        {
+            InitializeRecognizers();
+        }
+
+        void OnSwipeRecognized(TKSwipeRecognizer r)
+        {
+            Debug.Log("swipe recognized");
+        }
+
+        void OnTapRecognized(TKTapRecognizer r)
+        {
+            Debug.Log("tap recognized");
+        }
+
+        void OnLongTapRecognized(TKLongPressRecognizer r)
+        {
+            Debug.Log("long press started");
+        }
+
+        void OnLongTapFinished(TKLongPressRecognizer r)
+        {
+            Debug.Log("long press finished");
+        }
+
+        void OnPinchRecognized(TKPinchRecognizer r)
+        {
+            Debug.Log("pinch recognized");
+        }
+    }
+}
