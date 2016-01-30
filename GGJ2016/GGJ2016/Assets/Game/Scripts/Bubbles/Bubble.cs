557bc79f3bec05e76546cb9469a01528e57558b2
aries sanchez sulit <aries.sulit@gmail.com>
2016-01-30 19:26:52 +0800

    Added BubbleMove component

---
+++ GGJ2016/GGJ2016/Assets/Game/Scripts/Bubbles/Bubble.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using URandom = UnityEngine.Random;
+
+using System;
+using System.Collections;
+
+using Common;
+using Common.Signal;
+
+public enum EBubbleType {
+	Invalid		= 0x0,
+
+	SwipeUp		= 0x1 << 0,
+	SwipeDown	= 0x1 << 1,
+	SwipeLeft	= 0x1 << 2,
+	SwipeRight	= 0x1 << 3,
+
+	Tap			= 0x1 << 4,
+	Pinch		= 0x1 << 5,
+	LongPress	= 0x1 << 6,
+
+	Max			= 0x1 << 7,
+};
+
+namespace Game {
+
+	public interface IBubble {
+		EBubbleType BubbleType { get; }
+	};
+
+	public class Bubble : MonoBehaviour, IBubble {
+
+		[SerializeField]
+		private EBubbleType bubbleType;
+
+		// fade
+		[SerializeField]
+		private float fadeDuration = 2.0f;
+
+		private bool popped = false;
+
+		private void Awake() {
+		}
+
+		private void Start() {
+			this.StartCoroutine(this.Fade());
+		}
+
+		private void OnEnable() {
+			this.bubbleType = (EBubbleType)(0x1 << URandom.Range(0, 7));
+
+			// add to pool
+			Signal signal = GameSignals.ON_BUBBLE_ADDED_TO_POOL;
+			signal.AddParameter(GameParams.BUBBLE, this.gameObject);
+			signal.Dispatch();
+		}
+
+		private IEnumerator Fade() {
+			while (!this.popped) {
+				yield return null;
+			}
+
+			yield return new WaitForSeconds(5.0f);
+			GameObject.Destroy(this.gameObject);
+		}
+
+		public void Pop() {
+			this.popped = true;
+			this.GetComponent<BubbleMove>().enabled = false;
+		}
+
+		public EBubbleType BubbleType {
+			get {
+				return this.bubbleType;
+			}
+		}
+	}
+
+}
--- GGJ2016/GGJ2016/Assets/Game/Scripts/Bubbles/BubbleMove.cs
+++ GGJ2016/GGJ2016/Assets/Game/Scripts/Bubbles/BubbleMove.cs
@@ -7,28 +7,9 @@ using System.Collections;
 using Common;
 using Common.Signal;
 
-public enum EBubbleType {
-	Invalid		= 0x0,
-
-	SwipeUp		= 0x1 << 0,
-	SwipeDown	= 0x1 << 1,
-	SwipeLeft	= 0x1 << 2,
-	SwipeRight	= 0x1 << 3,
-
-	Tap			= 0x1 << 4,
-	Pinch		= 0x1 << 5,
-	LongPress	= 0x1 << 6,
-
-	Max			= 0x1 << 7,
-};
-
 namespace Game {
 
-	public interface IBubble {
-		EBubbleType BubbleType { get; }
-	};
-
-	public class Bubble : MonoBehaviour, IBubble {
+	public class BubbleMove : MonoBehaviour {
 		public const float CAP_Y_POSITION = 400.0f;
 
 		[SerializeField]
@@ -46,18 +27,12 @@ namespace Game {
 		private float scaleDuration;
 		private float totalTime;
 
-		// fade
-		[SerializeField]
-		private float fadeDuration = 2.0f;
-
 		[SerializeField]
 		[Range(0, 1)]
 		private int direction = -1;
 
 		private Vector3 axis;
 		private Vector3 pos;
-
-		private bool popped = false;
 
 		private void Awake() {
 			this.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
@@ -68,7 +43,6 @@ namespace Game {
 			this.axis = this.transform.right;
 			this.Move();
 			this.Scale();
-			this.StartCoroutine(this.Fade());
 		}
 
 		private void OnEnable() {
@@ -77,43 +51,24 @@ namespace Game {
 			this.frequency = URandom.Range(5.0f, 5.0f);
 			this.magnitude = URandom.Range(50.0f, 100.0f);
 			this.direction = URandom.Range(0, 2);
-			
+
 			this.totalTime = 0.0f;
 			this.scaleDuration = URandom.Range(0.15f, 0.35f);
 			this.transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
-
-			this.bubbleType = (EBubbleType)(0x1 << URandom.Range(0, 7));
-
-			// add to pool
-			Signal signal = GameSignals.ON_BUBBLE_ADDED_TO_POOL;
-			signal.AddParameter(GameParams.BUBBLE, this.gameObject);
-			signal.Dispatch();
 		}
 
 		private void FixedUpdate() {
-			if (!this.popped) {
-				this.Move();
-				this.Scale();
-			}
-			//else {
-			//	this.Fade();
-			//}
+			this.Move();
+			this.Scale();
 		}
 
 		private void LateUpdate() {
-			//Debug.LogFormat("Bubble::LateUpdate Y:{0}\n", this.transform.position.y);
 			if (this.transform.position.y >= CAP_Y_POSITION) {
-				// pop here
-				//GameObject.Destroy(this.gameObject);
-
 				// remove to pool
 				Signal signal = GameSignals.ON_BUBBLE_REMOVED_TO_POOL;
 				signal.AddParameter(GameParams.BUBBLE, this.gameObject);
 				signal.Dispatch();
 			}
-		}
-
-		private void OnDestroy() {
 		}
 
 		private void Move() {
@@ -128,7 +83,6 @@ namespace Game {
 		}
 
 		private void Scale() {
-			//this.transform.localScale = new Vector3(ranScale, ranScale, ranScale);
 			this.totalTime += Time.fixedDeltaTime;
 			float scale = this.totalTime / this.scaleDuration;
 
@@ -138,29 +92,6 @@ namespace Game {
 			}
 
 			this.transform.localScale = new Vector3(scale, scale, scale);
-		}
-
-		private IEnumerator Fade() {
-			//this.totalTime += Time.fixedDeltaTime;
-			//float fade = (this.totalTime / this.fadeDuration) - this.fadeDuration;
-			//Debug.LogFormat("Fade... {0}\n", fade);
-			while (!this.popped) {
-				yield return null;
-			}
-
-			yield return new WaitForSeconds(this.fadeDuration);
-			GameObject.Destroy(this.gameObject);
-		}
-
-		public void Pop() {
-			this.popped = true;
-			this.totalTime = 0;
-		}
-
-		public EBubbleType BubbleType {
-			get {
-				return this.bubbleType;
-			}
 		}
 	}
 
