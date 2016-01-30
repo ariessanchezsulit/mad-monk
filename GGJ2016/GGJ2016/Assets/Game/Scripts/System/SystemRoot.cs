a8ed13301b3ba10a1458e8ec4eeef4caa21e9db5
aries sanchez sulit <aries.sulit@gmail.com>
2016-01-30 13:11:45 +0800

    Added initial system scene.

--- GGJ2016/GGJ2016/Assets/Game/Scripts/System/SystemRoot.cs
+++ GGJ2016/GGJ2016/Assets/Game/Scripts/System/SystemRoot.cs
@@ -4,16 +4,7 @@ using System.Collections;
 namespace Game {
 
 	public class SystemRoot : MonoBehaviour {
-
-		// Use this for initialization
-		void Start () {
 		
-		}
-		
-		// Update is called once per frame
-		void Update () {
-		
-		}
 	}
 		
 }
