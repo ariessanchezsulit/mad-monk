dec815817d99a8c1e8d15f9aad4528f329ba218b
aries sanchez sulit <aries.sulit@gmail.com>
2016-01-31 06:29:55 +0800

    Added bubble colours.

--- GGJ2016/GGJ2016/Assets/Game/Scripts/Bubbles/Bubble.cs
+++ GGJ2016/GGJ2016/Assets/Game/Scripts/Bubbles/Bubble.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using URandom = UnityEngine.Random;
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 using Common;
 using Common.Signal;
@@ -22,10 +24,26 @@ public enum EBubbleType {
 	Max			= 0x1 << 7,
 };
 
+public enum EBubbleColor {
+	Invalid		= 0x0,
+
+	Red			= 0x1 << 0,
+	Orange		= 0x1 << 1,
+	Yellow		= 0x1 << 2,
+	Green		= 0x1 << 3,
+	Blue		= 0x1 << 4,
+	Indigo		= 0x1 << 5,
+	Violet		= 0x1 << 6,
+	Pink		= 0x1 << 7,
+
+	Max			= 0x1 << 8,
+}
+
 namespace Game {
 
 	public interface IBubble {
 		EBubbleType BubbleType { get; }
+		EBubbleColor BubbleColor { get; }
 	};
 
 	public class Bubble : MonoBehaviour, IBubble {
@@ -33,17 +51,34 @@ namespace Game {
 		[SerializeField]
 		private EBubbleType bubbleType;
 
+		[SerializeField]
+		private EBubbleColor bubbleColor;
+
+		[SerializeField]
+		private Image[] colors;
+
+		private Dictionary<EBubbleColor, Image> images;
+
 		private bool popped = false;
 
 		private void Awake() {
+			Assertion.Assert(this.colors.Length > 0);
 			this.GetComponent<BubbleFade>().enabled = false;
+			this.images = new Dictionary<EBubbleColor, Image>();
+
+			// populate bubbles
+			for (int i = 0; i < this.colors.Length; i++) {
+				this.images.Add((EBubbleColor)(0x1 << i), this.colors[i]);
+			}
 		}
 
 		private void Start() {
+			this.images[this.bubbleColor].gameObject.SetActive(true);
 		}
 
 		private void OnEnable() {
 			this.bubbleType = (EBubbleType)(0x1 << URandom.Range(0, 7));
+			this.bubbleColor = (EBubbleColor)(0x1 << URandom.Range(0, 8));
 
 			// add to pool
 			Signal signal = GameSignals.ON_BUBBLE_ADDED_TO_POOL;
@@ -60,6 +95,12 @@ namespace Game {
 		public EBubbleType BubbleType {
 			get {
 				return this.bubbleType;
+			}
+		}
+
+		public EBubbleColor BubbleColor {
+			get {
+				return this.bubbleColor;
 			}
 		}
 	}
