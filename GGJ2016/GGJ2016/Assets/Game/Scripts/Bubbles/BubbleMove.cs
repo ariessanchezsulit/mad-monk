21d7f524cd327b7ec56c2eb219215f082c913503
aries sanchez sulit <aries.sulit@gmail.com>
2016-01-31 08:11:47 +0800

    limit bubble movement.

--- GGJ2016/GGJ2016/Assets/Game/Scripts/Bubbles/BubbleMove.cs
+++ GGJ2016/GGJ2016/Assets/Game/Scripts/Bubbles/BubbleMove.cs
@@ -10,7 +10,7 @@ using Common.Signal;
 namespace Game {
 
 	public class BubbleMove : MonoBehaviour {
-		public const float CAP_Y_POSITION = 400.0f;
+		//public const float CAP_Y_POSITION = 400.0f;
 		
 		[SerializeField]
 		private float speed = 200.0f;
@@ -18,6 +18,7 @@ namespace Game {
 		private float frequency = 5.0f;  // Speed of sine movement
 		[SerializeField]
 		private float magnitude = 100.0f;   // Size of sine movement
+		private float capPositionY = 400.0f;
 
 		// scale
 		[SerializeField]
@@ -49,10 +50,30 @@ namespace Game {
 
 		private void OnEnable() {
 			// random values
-			this.speed = URandom.Range(100.0f, 500.0f);
-			this.frequency = URandom.Range(5.0f, 5.0f);
-			this.magnitude = URandom.Range(50.0f, 100.0f);
-			this.direction = URandom.Range(0, 2);
+			if (Platform.IsMobileIOS() || Platform.IsMobileAndroid()) {
+				float scaler = 4.0f;
+
+				this.speed 			= URandom.Range(600.0f * 1.0f, 1200.0f * 1.0f);
+				this.frequency 		= URandom.Range(5.0f * 1.0f, 5.0f * 1.0f);
+				this.magnitude 		= URandom.Range(300.0f, 500.0f);
+				this.direction 		= URandom.Range(0, 2);
+				this.capPositionY 	= 2000.0f;
+
+				/*
+				this.speed *= scaler;
+				this.frequency *= scaler;
+				this.magnitude *= scaler;
+				this.capPositionY *= scaler;
+				*/
+			}
+			else {
+				this.speed = URandom.Range(100.0f, 500.0f);
+				this.frequency = URandom.Range(5.0f, 5.0f);
+				this.magnitude = URandom.Range(50.0f, 100.0f);
+				this.direction = URandom.Range(0, 2);
+				this.capPositionY = 400.0f;
+			}
+
 
 			this.totalTime = 0.0f;
 			this.scaleDuration = URandom.Range(0.15f, 0.35f);
@@ -67,7 +88,7 @@ namespace Game {
 		}
 
 		private void LateUpdate() {
-			if (this.transform.position.y >= CAP_Y_POSITION) {
+			if (this.transform.position.y >= this.capPositionY) {
 				// dispatch bubble missed
 				Signal signal = GameSignals.ON_BUBBLE_MISSED;
 				signal.Dispatch();
@@ -80,7 +101,7 @@ namespace Game {
 		}
 
 		private void Move() {
-			this.pos += this.transform.up * Time.fixedDeltaTime * 0.75f * this.speed;
+			this.pos += this.transform.up * Time.fixedDeltaTime  * this.speed;
 
 			if (this.direction > 0) {
 				this.transform.position = this.pos + this.axis * Mathf.Sin(Time.time * this.frequency) * this.magnitude;
