688e3aca526eaef9a7e918d7f1024cc990c55aa2
Aries Sanchez Sulit <aries.sulit@gmail.com>
2018-01-27 01:00:20 +0800

    Cleanup Networking Codes

--- GGJ2016/GGJ2016/Assets/Game/Scripts/System/SystemRoot.cs
+++ GGJ2016/GGJ2016/Assets/Game/Scripts/System/SystemRoot.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 namespace Game {
 
-	public class SystemRoot : GameScene {
+	public class SystemRoot : GameScene
+    {
 		void Start()
 		{
-			LoadSceneAdditive (EScene.World);
-			//LoadSceneAdditive (EScene.Bubbles);
-			LoadSceneAdditive (EScene.UI);
-		}
+            GameSignals.START_GAME.Dispatch();
+        }
 	}
-
 }
